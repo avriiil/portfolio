deda3518e46144783a72cbfb7133a3007f374842
rrpelgrim <richardpelgrim@gmail.com>
2021-02-07 20:32:31 +0100

    Add capstone to portfolio directory. Update readmes.

---
+++ 0_CAPSTONE_Conflict_Climate_Prediction/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This is my second capstone, exploring relationship between armed conflict and climate change.',
+    author='Richard Pelgrim',
+    license='MIT',
+)
---
+++ 3_sql_country_club/README.md
@@ -0,0 +1,3 @@
+# SQL Assignment
+
+This directory contains my work for one of the SQL assignments that were part of my Data Science Career Track at Springboard. The notebook contains queries in MySQL and SQLite to access a relational database of country club 1) facilities, 2) members and 3) bookings.
